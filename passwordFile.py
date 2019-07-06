34ef67fe2bfd1044bfaf1456939a44b3556aeaff
Srijith <srijiths1998@gmail.com>
2019-07-06 15:25:33 +0530

    Added an option to add new account and password

--- passwordFile.py
+++ passwordFile.py
@@ -21,3 +21,11 @@ if account in pwds:
     print('Password for '+account+' has been copied to clipboard.')
 else:
     print('Password for '+account+' does not exist.')
+    print('Press 1 to add password for this account or 0 to exit')
+    rep=int(input())
+    if rep == 0:
+        sys.exit()
+    elif rep == 1:
+        print('Enter the password for '+account+':')
+        pwds[account]=input()
+        print('Password has been stored.')
